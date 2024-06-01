a6e0bd4808c831a885d864ef7daa59f378549e38
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2024-06-01 14:24:56 -0400

    Force based movement is back, Ground check
    Spray power editable var, remove dash. Updated .gitignore to ignore tmp
    files.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,33 +1,35 @@
 extends RigidBody2D
 
-@export var speed: float = 20000;
+@export var speed: float = 25;
 @export var jump_power: float = 500;
+@export var spray_power: float = 500;
+
+@export var grounded: bool = false
+
+const mult = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	lock_rotation = true
+	$GroundCol.body_entered.connect(func(_area): grounded = true)
+	$GroundCol.body_exited.connect(func(_area): grounded = false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var move: Vector2 = Vector2.ZERO
+	print(grounded)
 	if Input.is_action_pressed("move_right"):
-		move += (Vector2.RIGHT * speed * delta)
-		if Input.is_action_just_pressed("dash"):
-			move = (Vector2.RIGHT * speed * 15 * delta)
+		apply_central_impulse(Vector2.RIGHT * speed * delta * mult)
 	elif Input.is_action_pressed("move_left"):
-		move += (Vector2.LEFT * speed * delta)
-		if Input.is_action_just_pressed("dash"):
-			move = (Vector2.LEFT * speed * 15 * delta)
+		apply_central_impulse(Vector2.LEFT * speed * delta * mult)
 	
-	if Input.is_action_just_pressed("move_jump"):
-		linear_velocity.y = -jump_power
+	if grounded && Input.is_action_just_pressed("move_jump"):
+		apply_central_impulse(Vector2.UP * jump_power * delta * mult)
 	
-	linear_velocity.x = move.x
-	if Input.is_action_pressed("water"):
+	if Input.is_action_pressed("spray"):
 		hose_spray(delta)
 		
 func hose_spray(delta: float):
 	var mouse_position = get_viewport().get_mouse_position()
 	var spray_direction = (mouse_position - global_position).normalized()
-	apply_central_force(-spray_direction * 5000)
+	apply_central_impulse(-spray_direction * spray_power * mult * delta)
 	print(spray_direction)
